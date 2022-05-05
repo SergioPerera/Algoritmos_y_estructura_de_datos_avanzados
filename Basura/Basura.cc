c27c9fad7bb5609ff48cf2cff30591d158c37d67
Sergio Perera Márquez <72299705+SergioPerera@users.noreply.github.com>
2022-05-05 22:06:45 +0000

    -

--- Basura/Basura.cc
+++ Basura/Basura.cc
@@ -2,5 +2,6 @@
 #include <string>
 int main() {
   std::cout << "Estoy programando en ipad" << std::endl;
+  std::cout << "Estoy cambiando un poco el archivo para joder xD" << std::endl;
   return(0);
 }

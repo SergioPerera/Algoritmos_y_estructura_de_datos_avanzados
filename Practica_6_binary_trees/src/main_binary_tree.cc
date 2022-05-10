9a43b958900567d7f691c31c1ced0e8e78f565f4
SergioPerera <alu0101394503@ull.edu.es>
2022-05-10 19:12:21 +0100

    BackUp

---
+++ Practica_6_binary_trees/src/main_binary_tree.cc
@@ -0,0 +1,19 @@
+/**
+* Universidad de La Laguna
+* Escuela Superior de Ingeniería y Tecnología
+* Grado en Ingeniería Informática
+* Asignatura: Algoitmos y estructura de datos avanzadas
+* Curso: 2º
+* Práctica: Árbol binario equilibrado
+* @author Sergio Perera Márquez
+* Correo: alu0101394503@ull.edu.es
+* @date 10/05/2022
+* @file main_binary_tree.cc
+*/
+
+#include "../include/node.h"
+
+int main () {
+  return(0);
+}
+

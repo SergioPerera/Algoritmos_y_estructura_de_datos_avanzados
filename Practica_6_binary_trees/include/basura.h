a2435e088cfdce3f7635be1d5ed6a4067e4f642f
SergioPerera <alu0101394503@ull.edu.es>
2022-05-10 19:31:34 +0100

    BackUp

---
+++ Practica_6_binary_trees/include/basura.h
@@ -0,0 +1,10 @@
+
+#include<iostream>
+#include <string>
+
+class loco {
+  public:
+  loco(std::string str = "valor por defecto") : str_(str) {};
+  std::string str_;
+  private:
+};
--- Practica_6_binary_trees/src/basura.cc
+++ Practica_6_binary_trees/src/basura.cc
@@ -1,14 +1,13 @@
 #include<iostream>
-
-class loco {
-  public:
-  void Func(std::string str = "valor por defecto") : str_(str) {};
-  private:
-  std::string str_;
-};
+#include "../include/basura.h"
 
 
 int main () {
+  loco lokaso{};
+  std::cout << "Creo por defecto -> " << lokaso.str_ << std::endl;
+
+  loco loquito {"valor puesto por mi"};
+  std::cout << "Creo con valor -> " << loquito.str_ << std::endl;
 
   return(0);
 }

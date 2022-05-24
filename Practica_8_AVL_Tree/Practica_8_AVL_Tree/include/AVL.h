e0b287550a209843b4d804d2a7fc0cfccda3dbca
SergioPerera <alu0101394503@ull.edu.es>
2022-05-24 18:32:42 +0100

    BackUp

---
+++ Practica_8_AVL_Tree/Practica_8_AVL_Tree/include/AVL.h
@@ -0,0 +1,28 @@
+/**
+* Universidad de La Laguna
+* Escuela Superior de Ingeniería y Tecnología
+* Grado en Ingeniería Informática
+* Asignatura: Algoitmos y estructura de datos avanzadas
+* Curso: 2º
+* Práctica: Árbol binario equilibrado
+* @author Sergio Perera Márquez
+* Correo: alu0101394503@ull.edu.es
+* @date 24/05/2022
+* @file AL.h
+*/
+
+#ifndef _AVL_H_
+#define _AVL_H_
+#include "../include/ABB.h"
+
+template<class Key>
+class AVL : public AB<Key> {
+  
+
+};
+
+
+
+
+
+#endif // _AVL_H_
